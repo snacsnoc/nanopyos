0f0260697395d18d475de0ff8492644c9fc1e8e4
Easton Elliott <easton@geekness.eu>
2024-03-21 01:17:14 -0700

    make mp_int mp_uint 64 bit

--- src/include/sys/types.h
+++ src/include/sys/types.h
@@ -4,7 +4,7 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-#define NULL ((void*)0)
+//#define NULL ((void*)0)
 
 typedef unsigned short      ushort_t;
 typedef unsigned char       uchar_t;
@@ -13,6 +13,10 @@ typedef unsigned long       ulong_t;
 typedef unsigned long long  ulonglong_t;
 
 typedef unsigned long       off_t;
+
+typedef int64_t mp_int_t;
+typedef uint64_t mp_uint_t;
+typedef long mp_off_t;
 
 #ifndef _SIZE_T
 #  define _SIZE_T
