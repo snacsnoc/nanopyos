a7272b131646dffd8635b68db10a6466796a5d41
Easton Elliott <easton@geekness.eu>
2024-03-24 03:32:59 -0700

    Add more complex Python example code,

--- src/ports/bare-arm/main.c
+++ src/ports/bare-arm/main.c
@@ -35,34 +35,33 @@ volatile uint8_t *uart = (uint8_t *) 0x09000000;
 
 
 static const char *demo_single_input =
-    "print('hello world!', list(x + 1 for x in range(10)), end='eol\\n')";
+        "print('hello world!', list(x + 1 for x in range(10)), end='eol\\n')";
 
-//static const char *demo_file_input =
-//    "import micropython\n"
-//    "\n"
-//    "print(dir(micropython))\n"
-//    "\n"
-//    "for i in range(10):\n"
-//    "    print('iter {:08}'.format(i))";
-
-
-
-
-static void do_str(const char *src, mp_parse_input_kind_t input_kind) {
-    nlr_buf_t nlr;
-    if (nlr_push(&nlr) == 0) {
-        // Compile, parse and execute the given string.
-        mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, src, strlen(src), 0);
-        qstr source_name = lex->source_name;
-        mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
-        mp_obj_t module_fun = mp_compile(&parse_tree, source_name, true);
-        mp_call_function_0(module_fun);
-        nlr_pop();
-    } else {
-        // Uncaught exception: print it out.
-        mp_obj_print_exception(&mp_plat_print, (mp_obj_t)nlr.ret_val);
-    }
-}
+static const char *demo_complex_input =
+        "def calculate_primes(limit):\n"
+        "    primes = []\n"
+        "    for num in range(2, limit + 1):\n"
+        "        for prime in primes:\n"
+        "            if num % prime == 0:\n"
+        "                break\n"
+        "        else:\n"
+        "            primes.append(num)\n"
+        "    return primes\n"
+        "\n"
+        "limit = 30\n"
+        "primes = calculate_primes(limit)\n"
+        "print('Primes up to {}:'.format(limit))\n"
+        "print(primes)\n"
+        "\n"
+        "squares = [x**2 for x in range(10)]\n"
+        "print('Squares of numbers from 0 to 9:')\n"
+        "print(squares)\n"
+        "\n"
+        "for i in range(10):\n"
+        "    if i % 2 == 0:\n"
+        "        print('{} is even'.format(i))\n"
+        "    else:\n"
+        "        print('{} is odd'.format(i))";
 
 
 extern void main(void) __attribute__((visibility("default")));
@@ -77,12 +76,33 @@ void print(const char *s) {
     }
 }
 
+
+static void do_str(const char *src, mp_parse_input_kind_t input_kind) {
+    nlr_buf_t nlr;
+    if (nlr_push(&nlr) == 0) {
+        // Compile, parse and execute the given string.
+        mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, src, strlen(src), 0);
+        qstr source_name = lex->source_name;
+        mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
+        mp_obj_t module_fun = mp_compile(&parse_tree, source_name, true);
+        mp_call_function_0(module_fun);
+        nlr_pop();
+    } else {
+        print("Exception caught");
+        // Uncaught exception: print it out.
+        mp_obj_print_exception(&mp_plat_print, (mp_obj_t)nlr.ret_val);
+    }
+}
+
+
+
+
 // Main entry point: initialise the runtime and execute demo strings.
 void main(void) {
     print("Boots");
     mp_init();
     do_str(demo_single_input, MP_PARSE_SINGLE_INPUT);
-    //do_str(demo_file_input, MP_PARSE_FILE_INPUT);
+    do_str(demo_complex_input, MP_PARSE_FILE_INPUT);
     mp_deinit();
 }
 
