4fe36be4729b8fc7e70a1b482fec4dd538553ac7
Easton Elliott <easton@geekness.eu>
2024-04-03 02:25:27 -0700

    Clean up

--- src/include/bare-arm/mphalport.h
+++ src/include/bare-arm/mphalport.h
@@ -1,9 +1,6 @@
-// empty file
-//extern int mp_interrupt_char;
 static inline mp_uint_t mp_hal_ticks_ms(void) {
     return 0;
 }
-
 void mp_hal_set_interrupt_char(int c);
-//static inline void mp_hal_set_interrupt_char(char c) {}
 #define mp_hal_pin_obj_t
+
