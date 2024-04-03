aa576daa319993ba28cf1765132dc92acc2e2da3
Easton Elliott <easton@geekness.eu>
2024-04-02 22:33:25 -0700

    Add snprintf

--- src/include/stdio.h
+++ src/include/stdio.h
@@ -4,6 +4,7 @@
 #ifndef __STDIO_H__
 #define __STDIO_H__
 #include "sys/reent.h"
+#include <stddef.h>  // For size_t
 #define EOF (-1)
 
 #ifndef NULL
@@ -14,5 +15,9 @@ typedef __FILE FILE;
 # define __FILE_defined
 #endif
 int printf(const char *, ...);
+
+
+
+int snprintf(char *str, size_t size, const char *format, ...);
 
 #endif /* __STDIO_H__ */
