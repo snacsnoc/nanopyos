59aa18c3565701d5f0332ee34847221af988e446
Easton Elliott <easton@geekness.eu>
2024-04-05 01:43:00 -0700

    Add simulated TXE and RXNE simulated flags

--- src/ports/bare-arm/system.c
+++ src/ports/bare-arm/system.c
@@ -23,23 +23,27 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include "bare-arm/system.h"
 
+// Simulated UART status flags
+volatile bool uart_txe_simulated = true; // Assume TXE is initially true (ready to send)
+volatile bool uart_rxne_simulated = false; // RXNE initially false (no data to read)
 
-
-//volatile uint8_t *uart = (uint8_t *) 0x09000000;
-
-//volatile uint8_t *uart_status = (uint8_t *)0x09000004;
 
 typedef struct {
     volatile uint32_t DR;   // Data Register at offset 0x00 from UART_BASE
     volatile uint32_t SR;   // Status Register at offset 0x04 from UART_BASE
     volatile uint32_t BRR;
     volatile uint32_t CR1;
+
+    // Simulated functions for setting/getting status flags
+    void (*set_txe)(bool state);
+
+    void (*set_rxne)(bool state);
 } periph_uart_t;
 
 #define UART ((periph_uart_t *)0x09000000)
@@ -50,7 +54,6 @@ typedef struct {
 #define UART_SR_RXNE (1U << 5)
 // TXE: Transmit data register empty
 #define UART_SR_TXE  (1U << 7)
-
 
 
 extern uint64_t _estack, _sidata, _sdata, _edata, _sbss, _ebss, _start;
@@ -64,6 +67,48 @@ const uint64_t isr_vector[] __attribute__((section(".isr_vector"))) = {
         (uint64_t) & _start,
 };
 
+// Simulated RXNE and TXE
+void uart_set_txe_simulated(bool state) {
+    uart_txe_simulated = state;
+}
+
+void uart_set_rxne_simulated(bool state) {
+    uart_rxne_simulated = state;
+}
+
+// Initializing the UART structure with simulated functions
+void uart_init() {
+    printc("\n\ninit uart flags\n\n");
+    UART->set_txe = uart_set_txe_simulated;
+    UART->set_rxne = uart_set_rxne_simulated;
+}
+
+// Simulated UART send character function
+void uart_send_char(char c) {
+    while (!uart_txe_simulated) {
+        // Wait for TXE to be ready to send
+        printc("waiting for data\n");
+    }
+    UART->DR = (uint32_t) c;
+    // Simulate the TXE flag being cleared after sending a character
+    UART->set_txe(false);
+    // Simulate the TXE flag being set again (ready for next character)
+    UART->set_txe(true);
+}
+
+// Simulated UART receive character function
+char uart_receive_char() {
+    UART->set_rxne(true);
+    printc("init waiting....\n");
+    while (!uart_rxne_simulated) {
+        printc("waiting for data\n");
+        continue;
+    }
+    //char receivedChar = (char) UART->DR; // Read char from data register
+    // Simulate the RXNE flag being cleared after reading a character
+    UART->set_rxne(false);
+    return (char) UART->DR;
+}
 
 // Reading a character from UART
 int uart_read_char(void) {
@@ -72,59 +117,51 @@ int uart_read_char(void) {
 }
 
 
-
 int mp_hal_stdin_rx_chr(void) {
     // Wait until data is available
-    while (!uart_has_data()) {
-        // Loop until data becomes available
+    while (!(UART->SR & UART_SR_RXNE)) {
+
+        continue; // Wait while the receive data register is empty.
     }
-    // Read and return the received data from the data register
-    // Masking with 0xFF ensures we only get the data byte
-    return UART->DR & 0xFF;
+    return (int) (UART->DR & 0xFF); // Read the received character.
 }
 
-
+void debug_print(const char *message, uint32_t value) {
+    printf("%s: 0x%X\n", message, value);
+}
 
 
 // Directly write the character to the UART data register
 void uart_putc(char c) {
+
     // Wait for the transmit data register to be empty
-   //while (!(UART->SR & UART_SR_TXE)) {}
+    //while (!(UART->SR & UART_SR_TXE)) {}
 
     // Write the character to the Data Register
-    UART->DR = (uint32_t)c;
-    //UART->SR &= ~UART_SR_TXE; // Force the TXE flag to a clear state
-    //UART->SR |= UART_SR_TXE; // Force the TXE flag to a set state
+    UART->DR = (uint32_t) c;
+    UART->SR &= ~UART_SR_TXE; // Force the TXE flag to a clear state
+    UART->SR |= UART_SR_TXE; // Force the TXE flag to a set state
 }
 
 void mp_hal_stdout_tx_strn(const char *s) {
     while (*s != '\0') {
-        uart_putc(*s);
+        pputchar(*s);
         s++;
     }
 }
-
-
-
 
 
 int uart_has_data(void) {
     return UART->SR & UART_SR_RXNE;
 }
 
-//int uart_has_data_a(void) {
-//    // Check the SR register's RXNE bit to see if data is available.
-//
-//    return (UART->SR & UART_SR_RXNE) != 0;
-//}
 
-
-
-void pputchar(char c) { UART->DR = (uint32_t)c; }
+void pputchar(char c) { UART->DR = (uint32_t) c; }
 
 void print(const char *s) {
     while (*s != '\0') {
-        uart_putc(*s);
+
+        uart_send_char(*s);
         s++;
     }
 }
@@ -133,7 +170,7 @@ void print(const char *s) {
 // Send string of given length
 void printc(const char *s) {
     while (*s) {
-        uart_putc(*s++);
+        pputchar(*s++);
     }
 }
 
@@ -154,6 +191,6 @@ void printc_hex(unsigned long value) {
 
 
     while (*buf_ptr) {
-        uart_putc(*buf_ptr++);
+        pputchar(*buf_ptr++);
     }
 }
